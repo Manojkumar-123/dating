6c24f2d031b6c99f64c338dc54efeb09c4091ec8
mk <manojkandhasamy001@gmail.com>
2025-03-11 18:33:35 +0530

    Initial Commit for API

---
+++ Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController(DataContext context) : ControllerBase
+{
+    public readonly DataContext _context = context;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+        var users = await _context.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id){
+        var user = await _context.Users.FindAsync(id);
+
+        if(user == null)
+            return NotFound();   
+        return user;
+    }
+
+}
